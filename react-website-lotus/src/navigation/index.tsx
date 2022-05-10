51b77bf2dfb1228fcd719ccf5eef258639bf2c96
Ariane Medina <ariane.medina@accenture.com>
2022-05-10 16:36:19 -0300

    deploy

--- react-website-lotus/src/navigation/index.tsx
+++ react-website-lotus/src/navigation/index.tsx
@@ -7,7 +7,7 @@ import Home from "../pages/Home";
 
 export default function Navigation() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/skincare" element={<Skincare />} />
         <Route path="/corpo" element={<Body />} />
